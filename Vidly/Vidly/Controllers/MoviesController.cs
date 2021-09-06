f326a51e9ef23bbd620b3ecb9390b8287c528646
rmoyaTI <r.moya@cesariglesias.com.do>
2021-09-06 17:17:20 -0400

    Route Changing

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -16,5 +16,28 @@ namespace Vidly.Controllers
 
             return View(movie);
         }
+        public ActionResult Edit(int id)
+        {
+            return Content("Id=" + id); 
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
+        public ActionResult ByReleaseData(int year, byte month)
+        {
+            return Content(year + "/" + month);
+        }
     }
 }
