814cdd504f9998748bed7dbf3064ba2f9d96d37f
roland <rolan@LAPTOP-MH1LJF79>
2021-09-06 21:54:21 -0400

    Agregar Views models y detalles.

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -21,34 +21,39 @@ namespace Vidly.Controllers
 
             var viewModel = new RandomMovieViewModel
             {
-                Movie = movie,
+                //Movie = movie,
                 Customers = customers
             };
 
             return View(viewModel);
         }
-        public ActionResult Edit(int id)
+
+        public ActionResult Index()
         {
-            return Content("Id=" + id); 
+
+            var movies = GetMovies();
+
+          return View(movies); 
         }
 
-        public ActionResult Index(int? pageIndex, string sortBy)
+        public ActionResult Details(int id)
         {
-            if (!pageIndex.HasValue)
+            var movie = GetMovies().FirstOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+
+        public IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
             {
-                pageIndex = 1;
-            }
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-
+                new Movie { Id = 1, Name = "Shrek!"},
+                new Movie { Id = 2, Name = "Avengers"}
+            };
         }
 
-        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
-        public ActionResult ByReleaseData(int year, byte month)
-        {
-            return Content(year + "/" + month);
-        }
     }
 }
